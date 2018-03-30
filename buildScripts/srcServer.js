44de729839842438b852e4f20e813d43c49f2d43
Nicholas Wong <34085667+Klezca@users.noreply.github.com>
2018-03-30 14:36:43 +1100

    Created a basic hard-coded json api.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -6,7 +6,7 @@ import config from '../webpack.config.dev';
 
 /* eslint-disable no-console */
 
-const port = 3000;
+const port = 4000;
 const app = express();
 const compiler = webpack(config);
 
@@ -19,10 +19,19 @@ app.get('/', function(req, res) {
    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+   // Hard coding for simplicity. Pretend this hits a real database
+   res.json([
+      {"id":1, "firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+      {"id":2, "firstName":"Tammy","lastName":"Norton","email":"norton@hotmail.com"},
+      {"id":3, "firstName":"Tina","lastName":"Lee","email":"leetina@aol.com"}
+   ]);
+});
+
 app.listen(port, function(err) {
    if (err) {
       console.log(err);
   } else {
       open('http://localhost:' + port);
   }
-})
+});

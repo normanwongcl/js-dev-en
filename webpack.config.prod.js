54559864370cbc764dbb67d6a827d187037a06f8
Nicholas Wong <34085667+Klezca@users.noreply.github.com>
2018-03-31 22:44:28 +1100

    Added cache busting to production config.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-
+import WebpackpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
@@ -15,14 +16,21 @@ export default {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+     // Generate an external css file with a hash in the filename
+     new ExtractTextPlugin('[name].[contenthash].css'),
+
+     // Hash files using Md5 so that their names change when the content changes.
+     new WebpackpackMd5Hash(),
+
      // Use CommonsChunkPlugin to create a separate bundle
      // of vendor libraries so that they're cached separately.
      new webpack.optimize.CommonsChunkPlugin({
         name: 'vendor'
      }),
+
      //Create HTML file that includes reference to bundled JS.
      new HtmlWebpackPlugin({
         template: 'src/index.html',
@@ -46,7 +54,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }

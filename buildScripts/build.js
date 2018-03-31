155cdd466dc3036354994d83f41983ed76aeb299
Nicholas Wong <34085667+Klezca@users.noreply.github.com>
2018-03-31 21:23:48 +1100

    Generate message on when npm build is run.

--- buildScripts/build.js
+++ buildScripts/build.js
@@ -12,5 +12,21 @@ webpack(webpackConfig).run((err, stats) => {
       console.log(chalk.red(err));
       return 1;
    }
+
+   const jsonStats = stats.toJson();
+
+   if (jsonStats.hasErrors) {
+      return jsonStats.erros.map(error => console.log(chalk.red(error)));
+   }
+   if (jsonStats.hasWarnings) {
+      console.log(chalk.yellow('Webpack generated the following warnings: '));
+      jsonStats.warnings.map(warning => console.log(chalk.yellow(warning)));
+   }
+
+   console.log(`Webpack stats: ${stats}`);
+
+   // if we got this far, the build is successful
+   console.log(chalk.green('Your app has been built for production and written to /dist'));
+
    return 0; //signify success
-})
+});

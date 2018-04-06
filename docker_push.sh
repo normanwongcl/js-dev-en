683803771c3a822c69bce13db9169b0fbfe713fc
Nicholas Wong <34085667+Klezca@users.noreply.github.com>
2018-04-06 21:10:49 +1000

    Deployment Test #37

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
 curl --request POST --header Content-Type:application/json --data {"build":true} --url
 https://registry.hub.docker.com/u/nich19032/js-dev-en/trigger/d619d8ab-1b7a-4794-869e-89af55382b3c/

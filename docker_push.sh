bc6b7377d5cd9fb26c80c4468a14fe70462d5130
Nicholas Wong <34085667+Klezca@users.noreply.github.com>
2018-04-06 21:42:13 +1000

    Deployment Test #40

--- docker_push.sh
+++ docker_push.sh
@@ -1,1 +1,1 @@
-curl -H "Content-Type:application/json" --data '{"build":true}' -X POST $ curl POST https://cloud.docker.com/api/build/v1/source/44e1c147-4941-4f95-bd13-3d88a91a3ce8/trigger/b037fcab-490c-41bc-963b-6a91362773b4/call/
+curl -H "Content-Type:application/json" --data '{"build":true}' -X POST $ curl POST $Docker_Cloud
